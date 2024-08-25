b2638cbbb2e6c1cdfea16af6d36ad8b1e54d7778
unknown <rubinbaidhya@gmail.com>
2024-08-25 19:16:17 +0545

    Added Notification Feature

--- lib/actions/room.actions.ts
+++ lib/actions/room.actions.ts
@@ -100,6 +100,24 @@ export const updateDocumentAccess = async ({
       usersAccesses,
     });
 
+    if (room) {
+      const notificationId = nanoid();
+
+      await liveblocks.triggerInboxNotification({
+        userId: email,
+        kind: "$documentAccess",
+        subjectId: notificationId,
+        activityData: {
+          userType,
+          title: `You have been granted ${userType} access to the document by ${updatedBy.name}`,
+          updatedBy: updatedBy.name,
+          avatar: updatedBy.avatar,
+          email: updatedBy.email,
+        },
+        roomId,
+      });
+    }
+
     revalidatePath(`/documents/${roomId}`);
     return parseStringify(room);
   } catch (error) {
