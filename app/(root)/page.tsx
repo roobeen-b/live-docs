5c8005f3db36f1e7702a932112df122bf6247cad
unknown <rubinbaidhya@gmail.com>
2024-08-25 17:53:28 +0545

    Added document delete feature

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -8,6 +8,7 @@ import React from "react";
 import { getDocuments } from "@/lib/actions/room.actions";
 import Link from "next/link";
 import { dateConverter } from "@/lib/utils";
+import DeleteModal from "@/components/DeleteModal";
 
 const Home = async () => {
   const clerkUser = await currentUser();
@@ -58,6 +59,7 @@ const Home = async () => {
                       </p>
                     </div>
                   </Link>
+                  <DeleteModal roomId={id} />
                 </li>
               ))}
             </ul>
